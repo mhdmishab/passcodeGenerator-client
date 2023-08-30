399d59b14750388cc4c61a9c481efd842727fc7b
mhdmishab <mishabbtt63@gmail.com>
2023-08-30 13:47:54 +0530

    corsupdated

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
- const BASE_URL = 'http://127.0.0.1:8080/api' ;
-//  const BASE_URL ='https://passcrafter-x.onrender.com/api/v1/'
+//  const BASE_URL = 'http://127.0.0.1:8080/api' ;
+ const BASE_URL ='https://passcodegeneratorserver.onrender.com'
 
 
 const instance = axios.create({
